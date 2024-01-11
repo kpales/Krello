64c2362d57e25a9f33bf2b1504fbb2d78164f525
Ales Petrovacki <ales.petrovacki@kdab.com>
2024-01-11 13:07:31 -0500

    Create Trello Clone , WIP

---
+++ app.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <Html class="bg-emerald-500">
+    <div>
+      <NuxtPage />
+    </div>
+  </Html>
+</template>
---
+++ components/BoardColumn.vue
@@ -0,0 +1,75 @@
+<script setup>
+import { useBoardStore } from "../stores/BoardStore";
+
+const props = defineProps({
+  column: {
+    type: Object,
+    required: true,
+  },
+  columnIndex: {
+    type: Number,
+    required: true,
+  },
+});
+
+const boardStore = useBoardStore();
+const router = useRouter();
+const newTaskName = ref("");
+
+const editNameStates = ref(false);
+
+function addTask() {
+  boardStore.addTask({
+    taskName: newTaskName.value,
+    columnIndex: props.columnIndex,
+  });
+  newTaskName.value = "";
+}
+
+function deleteColumn(columnIndex) {
+  boardStore.deleteColumn(columnIndex);
+}
+
+function goToTask(taskId) {
+  router.push(`/tasks/${taskId}`);
+}
+</script>
+
+<template>
+  <UContainer class="column">
+    <div class="column-header mb-4">
+      <div>
+        <UInput v-if="editNameStates" type="text" v-model="column.name" />
+        <h2 v-else>{{ column.name }}</h2>
+      </div>
+      <div>
+        <UButton
+          icon="i-heroicons-pencil-square"
+          class="mr-2"
+          @click="editNameStates = !editNameStates"
+        />
+
+        <UButton
+          icon="i-heroicons-trash"
+          color="red"
+          @click="deleteColumn(columnIndex)"
+        />
+      </div>
+    </div>
+    <ul>
+      <li v-for="task in column.tasks" :key="task.id">
+        <UCard class="mb-4" @click="goToTask(task.id)">
+          <strong>{{ task.name }}</strong>
+          <p>{{ task.description }}</p>
+        </UCard>
+      </li>
+    </ul>
+    <UInput
+      v-model="newTaskName"
+      type="text"
+      placeholder="Create new task"
+      icon="i-heroicons-plus-circle-solid"
+      @keyup.enter="addTask"
+    />
+  </UContainer>
+</template>
